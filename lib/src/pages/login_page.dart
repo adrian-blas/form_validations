acddc41202cf3bfd29063081c91828aead66e2cc
militho <militho.blazitho@gmail.com>
2019-09-30 22:07:04 -0500

    cambios temporales

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -101,7 +101,7 @@ class LoginPage extends StatelessWidget {
               icon: Icon( Icons.alternate_email, color: Colors.deepPurple ),
               hintText: 'ejemplo.correo.com',
               labelText: 'Correo electrónico',
-              counterText: snapshot.data,
+              // counterText: snapshot.data,
               errorText: snapshot.error
             ),
             onChanged: bloc.changeEmail,
@@ -129,7 +129,7 @@ class LoginPage extends StatelessWidget {
             decoration: InputDecoration(
               icon: Icon( Icons.lock_outline, color: Colors.deepPurple ),
               labelText: 'Contraseña',
-              counterText: snapshot.data,
+              // counterText: snapshot.data,
               errorText: snapshot.error
             ),
             onChanged: bloc.changePassword,
